a01262faf849bdf91243f5f660e5bb9d9adb424f
Naoya Maeda <57132613+maeda-naoya-fk@users.noreply.github.com>
2020-06-24 16:47:33 +0900

    Update methods.py

--- methods.py
+++ methods.py
@@ -13,41 +13,50 @@ class Winning(Connect):
         self.df = self.connect()
         self.odds = self.df['オッズ']
         self.l = len(self.df)
+        #　pulpで使う時の変数を設定する
         self.V = [LpVariable('v'+n, lowBound = 0) for n in self.df.index]
-
+    
     def model_0(self):
-
+    
+        # ベット枚数を最小にする
         model = LpProblem(name = 'horse', sense = LpMinimize)
-
         model += lpSum(v for v in self.V)
 
         for l in range(self.l):
+            # どの馬が1着になっても、総ベット枚数＊１００円を超えるようにする
             model += self.odds[l] * self.V[l] - lpSum(v for v in self.V) >= 0
+            # ベット枚数は最小でも１枚はかける
             model += self.V[l] >= 1
 
         return model
 
     def model_1(self, betsum):
-
+        
+        # 総払い戻し金が最大になるようにする
         model = LpProblem(name = 'horse', sense = LpMaximize)
-
         odds_V = np.sum(self.odds * self.V)
         sum_V = lpSum(v for v in self.V)
-
         model += odds_V - self.l * sum_V
+        
+        # 総ベット枚数が最初に引数で渡したベット枚数を超えないようにする
         model += sum_V <= betsum
+        
         for l in range(self.l):
+            # どの馬が1着になっても、総ベット枚数＊１００円を超えるようにする
             model += self.odds[l] * self.V[l] - lpSum(v for v in self.V) >= 0
+            # ベット枚数は最小でも１枚はかける
             model += self.V[l] >= 1
 
         return model
-
+    
+    # 結果を表示
     def result(self, model):
 
         res = model.solve()
 
         bet = np.array([v.value() for v in self.V])
-
+        
+        #　ベット数は小数点になるので切り上げる
         bet = np.ceil(bet)
         result_betsum = np.sum(bet)
         if_win = self.odds * bet * 100
@@ -61,7 +70,7 @@ class Winning(Connect):
 
         return self.df, status
 
-
+# 期待値を出す
 class Expect(Connect):
 
     def __init__(self, tansyo, umatan_ = None, umatan__ = None, propability = None):
@@ -71,34 +80,42 @@ class Expect(Connect):
         self.l = len(self.df)
         self.odds = self.df['オッズ']
         self.pro = propability
+        # 確率を引数に持たせない場合オッズから計算したものを使う
         if self.pro == None:
             self.pro = self.df['確率']
         self.V = [LpVariable('v'+n, lowBound = 0) for n in self.df.index]
 
     def model_0(self, betsum, k):
-
+        
+        # 期待値を最大にする
         model = LpProblem(name = 'horse', sense = LpMaximize)
-
         model += lpSum(o * v * p for o, p, v in zip(self.odds, self.pro, self.V))
+        
+        # 総ベット枚数が最初に引数で渡したベット枚数を超えないようにする
         model += lpSum(v for v in self.V) <= betsum
-
+        
+        
         for i in range(self.l):
+            # ベット枚数は最小でも１枚はかける
             model += self.V[i] >= k
 
         return model
 
     def model_1(self, betsum):
 
+        # 期待値を最大にする
         model = LpProblem(name = 'horse', sense = LpMaximize)
-
         model += lpSum(o * v * p for o, p, v in zip(self.odds, self.pro, self.V))
+        
+        # 総ベット枚数が最初に引数で渡したベット枚数を超えないようにする
         model += lpSum(v for v in self.V) <= betsum
 
         pre_df = self.df.copy()
         pre_df['V'] = self.V
         pre_df = pre_df.sort_values('オッズ')
         V = pre_df['V']
-
+        
+        # オッズが高いものからベット枚数を大きくしていく　オッズが一番高いものでも最小でも１枚はかける
         for i in range(self.l):
             if i < self.l - 1:
                 model += V[i] - V[i+1] >= 1
@@ -107,7 +124,7 @@ class Expect(Connect):
 
         return model
 
-
+    # 結果を出力
     def result(self, model):
 
         result = model.solve()
